d060bd82c10bb43c80dd3c74cbf9427e800c7187
221hussein <yuzinpream51@gmail.com>
2023-10-11 22:40:23 +0000

    logout

--- src/main/java/com/hussein221/controller/AuthController.java
+++ src/main/java/com/hussein221/controller/AuthController.java
@@ -71,4 +71,16 @@ public class AuthController {
     public RefreshResponse refresh (@CookieValue("refresh_token")String refreshToken ) {
         return new RefreshResponse(authService.refreshAccess(refreshToken).getAccessToken().getToken());
     }
+
+    record LogoutResponse(String message) {}
+    @PostMapping("/logout")
+    public LogoutResponse logout(@CookieValue("refresh_token") String refreshToken, HttpServletResponse response) {
+        Cookie cookie = new Cookie("refresh_token",null);
+        cookie.setMaxAge(0);
+        cookie.setHttpOnly(true);
+
+        response.addCookie(cookie);
+
+        return new LogoutResponse("success");
+    }
 }
